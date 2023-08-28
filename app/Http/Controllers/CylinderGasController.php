c647ef8330563f3187c244030f52b8f9b3b05a15
atiqisrak <atiqisrak@gmail.com>
2023-08-28 16:44:51 +0600

    CylinderGas fixed

--- app/Http/Controllers/CylinderGasController.php
+++ app/Http/Controllers/CylinderGasController.php
@@ -62,8 +62,6 @@ class CylinderGasController extends Controller
     // 'advantages.*' => 'string',
     'status' => 'required|boolean',
         ]);
-
-        $fids = $validatedData['form_ids'];
         
         $validatedData['tabs'] = json_encode($validatedData['tabs']);
         $validatedData['cards_id'] = json_encode($validatedData['cards_id']);
@@ -100,43 +98,45 @@ class CylinderGasController extends Controller
 
         $validatedData = $request->validate([
             'title_en' => 'required|string',
-    'title_bn' => 'required|string',
-    'media_id' => 'required|integer|exists:media,id',
-    'tabs' => 'required|array',
-    'tabs.*.id' => 'required|integer',
-    'tabs.*.title_en' => 'required|string',
-    'tabs.*.title_bn' => 'required|string',
-    'card_id' => 'nullable|integer|exists:cards,id',
-    'media2_id' => 'nullable|integer|exists:media,id',
-    'card2_id' => 'nullable|integer|exists:cards,id',
-    'title2_en' => 'required|string',
-    'title2_bn' => 'required|string',
-    'subtitle_en' => 'required|string',
-    'subtitle_bn' => 'required|string',
-    'cards_id' => 'required|array',
-    'cards_id.*' => 'integer|exists:cards,id',
-    'title3_en' => 'required|string',
-    'title3_bn' => 'required|string',
-    'subtitle2_en' => 'required|string',
-    'subtitle2_bn' => 'required|string',
-    'cards2_id' => 'required|array',
-    'cards2_id.*' => 'integer|exists:cards,id',
-    'form_ids' => 'required|array',
-    'form_ids.*' => 'integer|exists:forms,id',
-    'title4_en' => 'required|string',
-    'title4_bn' => 'required|string',
-    'description_en' => 'required|string',
-    'description_bn' => 'required|string',
-    'title5_en' => 'required|string',
-    'title5_bn' => 'required|string',
-    'advantages' => 'required|array',
-    'advantages.*' => 'string',
-    'status' => 'required|boolean',
+            'title_bn' => 'required|string',
+            'media_id' => 'required|integer|exists:media,id',
+            'tabs' => 'required|array',
+            'tabs.*.id' => 'required|integer',
+            'tabs.*.title_en' => 'required|string',
+            'tabs.*.title_bn' => 'required|string',
+            'card_id' => 'nullable|integer|exists:cards,id',
+            'media2_id' => 'nullable|integer|exists:media,id',
+            'card2_id' => 'nullable|integer|exists:cards,id',
+            'title2_en' => 'required|string',
+            'title2_bn' => 'required|string',
+            'subtitle_en' => 'required|string',
+            'subtitle_bn' => 'required|string',
+            'cards_id' => 'required|array',
+            'cards_id.*' => 'integer|exists:cards,id',
+            'title3_en' => 'required|string',
+            'title3_bn' => 'required|string',
+            'subtitle2_en' => 'required|string',
+            'subtitle2_bn' => 'required|string',
+            'cards2_id' => 'required|array',
+            'cards2_id.*' => 'integer|exists:cards,id',
+            'form_ids' => 'required|array',
+            'form_ids.*' => 'integer|exists:forms,id',
+            'title4_en' => 'required|string',
+            'title4_bn' => 'required|string',
+            'description_en' => 'required|string',
+            'description_bn' => 'required|string',
+            'title5_en' => 'required|string',
+            'title5_bn' => 'required|string',
+            'advantages' => 'required|array',
+            'advantages.*' => 'string',
+            'status' => 'required|boolean',
         ]);
 
         $validatedData['tabs'] = json_encode($validatedData['tabs']);
         $validatedData['cards_id'] = json_encode($validatedData['cards_id']);
         $validatedData['cards2_id'] = json_encode($validatedData['cards2_id']);
+        $validatedData['form_ids'] = json_encode($validatedData['form_ids']);
+        $validatedData['advantages'] = json_encode($validatedData['advantages']);
 
         $cylinderGas->update($validatedData);
 
